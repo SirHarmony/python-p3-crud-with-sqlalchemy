a42c8a097e9a512f87de469a107148d75d00819b
Harmony <harmonymukolwe@gmail.com>
2024-09-17 22:04:14 +0300

    Performs CRUD operations

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime
 
-from sqlalchemy import (create_engine, desc, func,
+from sqlalchemy import (create_engine, desc,
     CheckConstraint, PrimaryKeyConstraint, UniqueConstraint,
     Index, Column, DateTime, Integer, String)
 from sqlalchemy.ext.declarative import declarative_base
@@ -12,10 +12,137 @@ Base = declarative_base()
 
 class Student(Base):
     __tablename__ = 'students'
+    __table_args__ = (
+        PrimaryKeyConstraint(
+            'id',
+            name='id_pk'),
+        UniqueConstraint(
+            'email',
+            name='unique_email'),
+        CheckConstraint(
+            'grade BETWEEN 1 AND 12',
+            name='grade_between_1_and_12'))
 
-    id = Column(Integer(), primary_key=True)
+    Index('index_name', 'name')
+
+    id = Column(Integer())
     name = Column(String())
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default=datetime.now())
+
+    def __repr__(self):
+        return f"Student {self.id}: " \
+            + f"{self.name}, " \
+            + f"Grade {self.grade}"
 
 if __name__ == '__main__':
+
     engine = create_engine('sqlite:///:memory:')
     Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    albert_einstein = Student(
+        student_name="Albert Einstein",
+        student_email="albert.einstein@zurich.edu",
+        student_grade=6,
+        student_birthday=datetime(
+            year=1879,
+            month=3,
+            day=14
+        ),
+    )
+
+    alan_turing = Student(
+        student_name="Alan Turing",
+        student_email="alan.turing@sherborne.edu",
+        student_grade=11,
+        student_birthday=datetime(
+            year=1912,
+            month=6,
+            day=23
+        ),
+    )
+
+    session.bulk_save_objects([albert_einstein, alan_turing])
+    session.commit()
+
+# Read Records
+# create session, student objects
+students = session.query(Student)
+print([student for student in students])
+
+students = session.query(Student).all()
+print(students)
+
+# Selecting only certain columns
+# create session, student objects
+names = [name for name in session.query(Student.name)]
+print(names)
+
+# Ordering
+# create session, student objects
+students_by_name = [student for student in session.query(
+            Student.name).order_by(
+            Student.name)]
+print(students_by_name)
+
+# Limiting
+# create session, student objects
+oldest_student = [student for student in session.query(
+            Student.name, Student.birthday).order_by(
+            desc(Student.grade)).limit(1)]
+print(oldest_student)
+
+# Filtering
+# create session, student objects
+query = session.query(Student).filter(Student.name.like('%Alan%'),
+        Student.grade == 11)
+for record in query:
+        print(record.name)
+
+# Updating data
+# create session, student objects
+for student in session.query(Student):
+        student.grade += 1
+session.commit()
+print([(student.name,
+        student.grade) for student in session.query(Student)])
+
+# Updating data using update()
+# create session, student objects
+session.query(Student).update({
+        Student.grade: Student.grade + 1
+    })
+
+print([(
+      student.name,
+      student.grade
+      ) for student in session.query(Student)])
+
+# Deleting data
+# create session, student objects
+query = session.query(
+        Student).filter(
+            Student.name == "Albert Einstein")        
+# retrieve first matching record as object
+albert_einstein = query.first()
+# delete record
+session.delete(albert_einstein)
+session.commit()
+# try to retrieve deleted record
+albert_einstein = query.first()
+print(albert_einstein)
+
+# Deleting all records returned by your query
+# create session, student objects
+query = session.query(
+        Student).filter(
+            Student.name == "Albert Einstein")
+query.delete()
+albert_einstein = query.first()
+print(albert_einstein)
+# => None
